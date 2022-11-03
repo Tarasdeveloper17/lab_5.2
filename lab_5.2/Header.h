87571d4b3c66ee80f17351e3e7c5ac2eda0b5083
Taras <taras.horta@gmail.com>
2022-11-03 22:46:01 +0200

    added project

---
+++ lab_5.2/Header.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  lab_5.2
+//
+//  Created by Тарас Могила on 03.11.2022.
+//
+
+#ifndef Header_h
+#define Header_h
+#include "main.cpp"
+
+#endif /* Header_h */
---
+++ unit_test_5.2/unit_test_5_2.mm
@@ -0,0 +1,20 @@
+
+#import "Header.h"
+#import <XCTest/XCTest.h>
+
+@interface popop : XCTestCase
+
+@end
+
+@implementation popop
+
+- (void)testExample {
+    
+    double resultFunc =  2. * (1./1);
+    
+    XCTAssertEqual(resultFunc, 2);
+}
+
+
+@end
+
